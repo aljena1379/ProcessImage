9edbd2115375c0f421540a1a4fe82a347b404809
Aljena <aljena.3713@gmail.com>
2024-03-13 10:23:00 +0330

    add TrainedData to tesseract

--- RunTesseract.bat
+++ RunTesseract.bat
@@ -1,3 +1,3 @@
 @echo off
 @REM TODO: make this function operate for every image given as input
-tesseract testImg.png testText -l ara
+tesseract --tessdata-dir C:\Program Files\Tesseract-OCR\tessdata testImg.png testText -l ara
